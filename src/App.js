a98e12b9eb7d1f632cc9d20039cd324c4cafedd7
Joey <legerinvestments@gmail.com>
2020-04-10 20:02:13 -0400

    feat: temp color scale working

--- src/App.js
+++ src/App.js
@@ -34,14 +34,55 @@ const App = () => {
     );
   };
 
+  const RenderCellColor = (num) => {
+    const rating = Number(num);
+    // Great - green - 8.6 up
+    // Good - yellow - 7.6 up
+    // Regular - orange - 6.6 up
+    // Bad - red - 5.0 up
+    // Garbage - blue - 4.9 down
+    if (rating >= 8.6) {
+      return 'green';
+    }
+    if (rating >= 7.6 && rating <= 8.5) {
+      return 'yellow';
+    }
+    if (rating >= 6.6 && rating <= 7.5) {
+      return 'orange';
+    }
+    if (rating >= 5.0 && rating <= 6.5) {
+      return 'red';
+    }
+    if (rating <= 4.9) {
+      return 'blue';
+    }
+    return null;
+  };
+
+  const RenderBlankCells = (raw) => {
+    if (mostEpisodes !== null) {
+      const total = mostEpisodes - raw.length;
+      if (total > 0) {
+        return total;
+      }
+      return null;
+    }
+    return null;
+  };
+
   const RenderSeason2 = (obj, idx) => {
+    const blankCells = RenderBlankCells(obj);
+
     return (
       <div className='app__column' key={idx}>
         {obj.map((elm2, index) => (
-          <div className='app__cell' key={index}>
+          <div className={`app__cell app__cell--${RenderCellColor(elm2.imdbRating)}`} key={index}>
             {elm2.imdbRating}
           </div>
         ))}
+        {blankCells !== null
+          ? Array.from(Array(blankCells)).map((x, idx) => <div className='app__cell' key={idx} />)
+          : null}
       </div>
     );
   };
