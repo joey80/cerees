2264ef24ca89007ded240278ebf9dd408929d872
jleger <j.leger@jobtarget.com>
2023-04-25 22:48:54 -0400

    Updated EpisodeModal to only display certain fields. Updated its styling

--- src/components/EpisodeModal/index.tsx
+++ src/components/EpisodeModal/index.tsx
@@ -1,7 +1,15 @@
-import { PropsWithChildren } from 'react';
+import { IGetEpisode } from '@/pages/api/omdb/getEpisode/types';
 
-function EpisodeModal({ children }: PropsWithChildren<Record<string, unknown>>) {
-  return <div className="absolute bg-white overflow-hidden border-2 border-black top-0 mt-14 w-80 h-56 z-10 text-sm">{children}</div>;
+function EpisodeModal({ Plot, Released, Title }: IGetEpisode) {
+  return (
+    <div className="absolute bg-white overflow-hidden border border-zinc-800 border-dashed top-0 mt-14 p-3 w-80 h-56 z-10 text-sm rounded-xl drop-shadow-xl">
+      <ul>
+        <li className="text-lg font-medium">{Title}</li>
+        <li className="pb-2 border-b">Released: {Released}</li>
+        <li className="pt-2">{Plot}</li>
+      </ul>
+    </div>
+  );
 }
 
 export { EpisodeModal };
