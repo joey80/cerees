dff42580a1411b7f9672bb652324853b286a3cbe
jleger <j.leger@jobtarget.com>
2023-04-26 22:46:57 -0400

    Updated components to use new colors and set body background

--- src/components/EpisodeModal/index.tsx
+++ src/components/EpisodeModal/index.tsx
@@ -2,7 +2,7 @@ import { IGetEpisode } from '@/pages/api/omdb/getEpisode/types';
 
 function EpisodeModal({ Plot, Released, Title }: IGetEpisode) {
   return (
-    <div className="absolute bg-white overflow-hidden border border-zinc-800 border-dashed top-0 mt-14 p-3 w-80 h-56 z-10 text-sm rounded-xl drop-shadow-xl">
+    <div className="absolute bg-white overflow-hidden border custom-border-color border-dashed top-0 mt-14 p-3 w-80 h-56 z-10 text-sm rounded-xl drop-shadow-xl">
       <ul>
         <li className="text-lg font-medium">{Title}</li>
         <li className="pb-2 border-b">Released: {Released}</li>
